a1d822dad5f52cc6e0905f5a12abc2db29d4f83b
vasquezluis <luivasquez95@gmail.com>
2023-04-13 10:15:39 -0600

    put & delete routes added

--- src/api/v1/routes/movies.routes.ts
+++ src/api/v1/routes/movies.routes.ts
@@ -3,6 +3,8 @@ import {
   getItem,
   getItems,
   createItem,
+  updateItem,
+  deleteItem,
 } from "../controllers/movies.controllers";
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.get("/api/v1/movies/:id", getItem);
 router.get("/api/v1/movies", getItems);
 router.post("/api/v1/movies", createItem);
+router.put("/api/v1/movies/id", updateItem);
+router.delete("/api/v1/movies/:id", deleteItem);
 
 export default router;
