70b946f929ed37e615152335c5281d1729be292f
vasquezluis <luivasquez95@gmail.com>
2023-04-13 10:19:08 -0600

    fixed put variable id

--- src/api/v1/routes/movies.routes.ts
+++ src/api/v1/routes/movies.routes.ts
@@ -12,7 +12,7 @@ const router = Router();
 router.get("/api/v1/movies/:id", getItem);
 router.get("/api/v1/movies", getItems);
 router.post("/api/v1/movies", createItem);
-router.put("/api/v1/movies/id", updateItem);
+router.put("/api/v1/movies/:id", updateItem);
 router.delete("/api/v1/movies/:id", deleteItem);
 
 export default router;
